724e662b36164277396d6bd373c80cd4325de0ec
krishnasaiu <krish.m8@gmail.com>
2016-05-15 20:01:38 +0530

    baseline

--- search.php
+++ search.php
@@ -2,21 +2,29 @@
 include('location.php');
 
 function get_url_contents($url) {
-        $crl = curl_init();
+  $crl = curl_init();
 	$customHeader = array('user-key: '.'7a23aa4614aecba1b3d05cb94d89540c', 'Accept: application/json');
-        $timeout = 5;
-        curl_setopt ($crl, CURLOPT_URL, $url);
-        curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+  $timeout = 5;
+
+  curl_setopt ($crl, CURLOPT_URL, $url);
+  curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($crl, CURLOPT_HTTPHEADER, $customHeader);
-        curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $ret = curl_exec($crl);
-        curl_close($crl);
-        return $ret;
+  curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+  $ret = curl_exec($crl);
+  curl_close($crl);
+
+  return $ret;
 }
 
-function getRestaurantsList($keywords, $location) {
-	$coordinates = getLatLon($location);	
-	$url = get_url_contents("https://developers.zomato.com/api/v2.1/search?q=".$keywords."&lat=".$coordinates['lat']."&lon=".$coordinates['lon']."&radius=5000&sort=real_distance");
+function getRestaurantsList($keywords, $location, $radius) {
+	$coordinates = getLatLon($location);
+	$url = get_url_contents("https://developers.zomato.com/api/v2.1/search?".
+                          "q=".$keywords.
+                          "&lat=".$coordinates['lat'].
+                          "&lon=".$coordinates['lon'].
+                          "&radius=".$radius.
+                          "&sort=real_distance");
 	$response = json_decode($url, true);
 	return $response['restaurants'];
 }
--- welcome.php
+++ welcome.php
@@ -1,46 +1,48 @@
 <?php
-   	include('session.php');
+  include('session.php');
 	include('search.php');
 
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		$keywords = $_POST['keywords'];
-		$costForTwo = $_POST['costForTwo'];
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $keywords = $_POST['keywords'];
+    $costForTwo = (int) $_POST['costForTwo'];
 		$location = $_POST['location'];
-		
-		$result = getRestaurantsList($keywords, $location);
+    $radius = ((int) $_POST['radius']) * 1000;
 
-		echo $keywords, $costForTwo, $location;
+		$result = getRestaurantsList($keywords, $location, $radius);
+
+		echo $keywords, $costForTwo, $location, $radius;
 		echo $result[0]['restaurant']['name'];
 	}
 ?>
 <html>
-   
-   	<head>
-      		<title>Welcome </title>
-	</head>
-   
-   	<body>
 
-      		<h1>Welcome <?php echo $login_session; ?>!!</h1> 
-      		<h2><a href = "logout.php">Sign Out</a></h2>
-   		
+  <head>
+    <title>Welcome</title>
+	</head>
+
+  <body>
+    <h1>Welcome <?php echo $login_session; ?>!!</h1>
+    <h2><a href="logout.php">Sign Out</a></h2>
+
 		<div>
-		<form action = "" method = "post">
-			<label>Keywords:</label></br>
-			<input type = "text" name = "keywords" style = "width: 50%"/> <br /> <br />
-			<label>Cost for two:</label>
-			<select name = "costForTwo">
-    				<option value="" disabled = "disabled" selected = "selected">-- select --</option>
-    				<option value = "500">500</option>
-    				<option value = "1000">1000</option>
-				<option value = "2000">2000</option>
-				<option value = "-1">∞</option>
-			</select><br /> <br />
-			<label>Location:</label> <br />
-			<input type = "text" name = "location" style = "width: 50%"/> <br />
-			<input type = "submit" value = " Submit "/><br />
-		</form>
+		    <form action="" method="post">
+          <label>Keywords:</label></br>
+			    <input type="text" name="keywords" style="width: 50%"/> <br /> <br />
+			    <label>Cost for two:</label>
+          <select name="costForTwo">
+            <option value="" disabled="disabled" selected="selected">-- select --</option>
+  				  <option value="500">500</option>
+            <option value="1000">1000</option>
+				    <option value="2000">2000</option>
+				    <option value="-1">∞</option>
+			    </select><br /> <br />
+			    <label>Location:</label> <br />
+          <input type="text" name="location" style="width: 50%"/> <br /> <br />
+          <label>How far are you willing to go?</label> <br />
+          <input type="text" name="radius" placeholder="inKM"/> <br />
+    	    <input type="submit" value="Submit "/><br />
+		    </form>
 		</div>
 	</body>
-   
+
 </html>
